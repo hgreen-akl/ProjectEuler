28e7850d08378b9550cd8f5ec7a9e6ad06bba84f
hgreen-akl <32467643+hgreen-akl@users.noreply.github.com>
2020-05-06 10:01:58 +1200

    Solved Problem 7

--- Python/Problem 7.py
+++ Python/Problem 7.py
@@ -4,16 +4,36 @@
 
 number = 7
 
-def is_prime(n):
-    if n < 2:
+def is_prime(n) :
+    if (n < 2):
         return False
-    elif n == 2:
+    elif (n==2):
         return True
-    if n > 2:
-        for i in range(n,n//2)":
-            if n % i == 0:
-                break
-        else:
-            return False
+    elif (n==3):
+        return True
+    else:
+        for x in range(2,n//2):
+            if(n % x==0):
+                return False
+        return True
 
-is_prime(number)
+def get_nth_prime(x):
+    """[Obtains the nth prime number based on a function that is prime]
+
+    Arguments:
+        x {[int]} -- [the nth prime number wanted]
+    """
+    number = 0
+    prime_list = []
+    while len(prime_list) <=  x:
+        if is_prime(number):
+            prime_list.append(number)
+            number += 1
+        else:
+            number +=1
+    return prime_list[-1]
+
+
+get_nth_prime(5) # this should be 11
+get_nth_prime(6) # this should be 13
+get_nth_prime(10001)
